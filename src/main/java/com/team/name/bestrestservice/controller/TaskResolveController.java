2eb93174f730abdd7e97b96c987c21c794bd98e2
nahodkins <tsuber2001@gmail.com>
2023-02-08 21:41:20 +0200

    Commit#7

--- src/main/java/com/team/name/bestrestservice/controller/TaskResolveController.java
+++ src/main/java/com/team/name/bestrestservice/controller/TaskResolveController.java
@@ -20,12 +20,78 @@ public class TaskResolveController {
 
     @PostMapping("/move")
     public ResponseEntity<MyResponseBody> move(@RequestBody GameState gameState) {
+        char opponent = getOpponent(gameState);
+        int vertical = checkVertical(opponent, gameState.getBoard());
+        int horisontal = checkHorizontal(opponent, gameState.getBoard());
+
+        if (vertical != -1) {
+            MyResponseBody responseBody = new MyResponseBody();
+            responseBody.setColumn(vertical);
+            return ResponseEntity.ok(responseBody);
+        }
+
+        if (horisontal != -1) {
+            MyResponseBody responseBody = new MyResponseBody();
+            responseBody.setColumn(horisontal);
+            return ResponseEntity.ok(responseBody);
+        }
+
         State state = new State(gameState.getBoard(), gameState.getPlayer());
         MiniMaxAgent miniMaxAgent = new MiniMaxAgent(getFirstEmptyRow(gameState.getBoard()));
         int action = miniMaxAgent.getAction(state);
         MyResponseBody myResponseBody = new MyResponseBody();
         myResponseBody.setColumn(action);
         return ResponseEntity.ok(myResponseBody);
+    }
+
+    private char getOpponent(GameState gameState) {
+        return gameState.getPlayer().charAt(0) == 'S'
+                ? 'F'
+                : 'S';
+    }
+
+    private int checkHorizontal(char opponent, char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            int count = 0;
+            var row = board[i];
+            for (int j = 0; j < row.length; j++) {
+                if (count == 3 && row[j] == '_') {
+                    return j;
+                }
+                if (row[j] == opponent) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            }
+        }
+        return -1;
+    }
+//        ["_","_","_","_","_","_","_","_","_"],
+//        ["S","_","_","_","X","F","_","_","_"],
+//        ["F","_","_","_","X","S","S","_","_"],
+//        ["F","_","_","_","X","S","F","S","_"],
+//        ["S","_","F","_","X","F","S","S","S"],
+//        ["F","_","F","_","X","F","S","F","S"]
+    private int checkVertical(char opponent, char[][] board) {
+        for (int i = board[0].length - 1; i >= 0; i--) {
+            int count = 0;
+            for (int j = board.length - 1; j >= 0; j--) {
+                if (i == 0) {
+                    return -1;
+                }
+                if (count == 3 && board[j][i - 1] == '_') {
+                    return i;
+                }
+                if (board[j][i] == opponent) {
+                    count++;
+                } else {
+                    count = 0;
+                }
+            }
+        }
+
+        return -1;
     }
 
     private int getFirstEmptyRow(char[][] board) {
