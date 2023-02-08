49116029ece8d67bdbabeff4bb93335da1f95049
nahodkins <tsuber2001@gmail.com>
2023-02-08 18:10:53 +0200

    Commit#1

--- src/main/java/com/team/name/bestrestservice/controller/TaskResolveController.java
+++ src/main/java/com/team/name/bestrestservice/controller/TaskResolveController.java
@@ -2,6 +2,8 @@ package com.team.name.bestrestservice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,12 @@ public class TaskResolveController {
 
     @GetMapping("/healthz")
     public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok("OK");
+    }
+
+    @PostMapping("/move")
+    public ResponseEntity<String> move(@RequestBody String request) {
+        System.out.println(request);
         return ResponseEntity.ok("OK");
     }
 }
